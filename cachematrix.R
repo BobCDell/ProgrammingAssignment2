ba45b6362cf3c0a9d82693436a5e7f65fa4bd9c9
BobCDell <Bob.Coon@software.dell.com>
2016-04-24 15:16:25 -0400

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix and cacheSolve create a special "matrix" object (makeCacheMatrix) that can
+##    cache its inverse which is then retrieved by cacheSolve.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat=matrix()){
+  #Description: 
+  #  Argument: mat - a square, invertible matrix 
+  #  Returns - a list of functions:
+  #             a. setmat - sets the matrix.
+  #             b. getmat - gets the matrix.
+  #             c. setinvrs - sets the inverse matrix.
+  #             d. getinvrs - gets the inverse matrix.
+  #           the above list of functions serves as the argument to the 
+  #           accompanying function for this assignment: cacheSolve()
+  
+  invrs <- NULL
+  setmat <- function(newmat) {
+    mat <<- newmat
+    #assign mat to newmat in a different environment using <<-
+    invrs <<- NULL
+    #assign invrs to NULL in a different environment using <<-
+  }
+  
+  getmat <- function() mat
+  setinvrs <- function(inverse) invrs <<- inverse
+  getinvrs <- function() invrs
+  list(setmat <- setmat, getmat <- getmat, setinvrs <- setinvrs, getinvrs <- getinvrs)      
 }
 
 
-## Write a short comment describing this function
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then 
+#cachesolve retrieves the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat,...) {
+  #Description:
+  # Argument: mat a matrix outputted from makeCacheMatrix()
+  # Return: the inverse of the original matrix used as the
+  #   argument for getCacheMatrix
+  
+  invrs = mat$getinvrs()
+  
+  #Search for the inverse to see if it already exists.
+  if (!is.null(invrs)){
+    #If it exists, retrieve it from the cache. 
+    
+    message("Searching for cached matrix.")
+    return(invrs)
+  }
+  
+  #If it does not exist, then calculate it.
+  
+  mat.data <- mat$getmat()
+  invrs <- solve(mat.data, ...)
+  
+  #Set the value of the cache inverse matrix 
+  mat$setinvrs(invrs)
+  
+  return(invrs)
 }
